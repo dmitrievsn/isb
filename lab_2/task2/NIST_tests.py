221d60b9db9d4f8d2afca876856c9eba544e7062
Laptop <dreyui4@gmail.com>
2025-04-01 01:01:32 +0400

    calculating statistics for blocks and test for the longest sequence of units in a block

--- lab_2/task2/NIST_tests.py
+++ lab_2/task2/NIST_tests.py
@@ -1,5 +1,8 @@
 import math
 
+from scipy.special import gammainc
+
+
 def read_txt_file(file_path: str) -> str:
     """
     A function for reading a text file.
@@ -35,3 +38,39 @@ def consecutive_bits_test(sequence:str) -> float:
     V = sum(sequence[i] != sequence[i + 1] for i in range(len(sequence) - 1))
     return math.erf(abs(V-2*len(sequence)*percentage_units*(1-percentage_units))/(2*math.sqrt(2*len(sequence))*percentage_units*(1-percentage_units)))
 
+
+def block_statistic(sequence:str) -> tuple:
+    """
+    Calculates statistic of identical bits in each block of 8 bits in the sequence
+    :param sequence: bitwise sequence
+    :return: statistic
+    """
+    V1, V2, V3, V4 = 0, 0, 0, 0
+    for i in range(0, len(sequence), 8):
+        max_len = 0
+        current_len = 0
+        for bit in sequence[i:i + 8]:
+            if bit == "1":
+                current_len += 1
+                max_len = max(max_len, current_len)
+            else:
+                current_len = 0
+        match max_len:
+            case 1: V1 += 1
+            case 2: V2 += 1
+            case 3: V3 += 1
+            case _: V4 += 1
+    return V1, V2, V3, V4
+
+
+def test_identical_consecutive_bits(statistic:tuple) -> float:
+    """
+    Test for the longest sequence of units in a block
+    :param statistic: statistic
+    :return: P-value
+    """
+    P = [0.2148, 0.3672, 0.2305, 0.1875]
+    chi_square = 0
+    for i in range(0, 4):
+        chi_square += ((statistic[i] - 16 * P[i]) ** 2) / (16 * P[i])
+    return gammainc(3 / 2, chi_square / 2)
