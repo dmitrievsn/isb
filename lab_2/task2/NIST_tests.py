e0f870721c0620a3c8cbc5dd300bf4b2c8179e82
Laptop <dreyui4@gmail.com>
2025-04-01 00:03:16 +0400

    a test for identical consecutive bits

--- lab_2/task2/NIST_tests.py
+++ lab_2/task2/NIST_tests.py
@@ -21,3 +21,17 @@ def frequency_test(sequence:str) -> float:
     """
     S=(sequence.count("1")-sequence.count("0"))/math.sqrt(len(sequence))
     return math.erf(S/math.sqrt(2))
+
+
+def consecutive_bits_test(sequence:str) -> float:
+    """
+    A test for identical consecutive bits
+    :param sequence:bitwise sequence
+    :return:P-value
+    """
+    percentage_units = sequence.count("1")/len(sequence)
+    if not(abs(percentage_units-1/2)<2/math.sqrt(len(sequence))):
+        return 0
+    V = sum(sequence[i] != sequence[i + 1] for i in range(len(sequence) - 1))
+    return math.erf(abs(V-2*len(sequence)*percentage_units*(1-percentage_units))/(2*math.sqrt(2*len(sequence))*percentage_units*(1-percentage_units)))
+
