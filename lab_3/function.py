713f123ab9fa51e73bb47bc820cc6f5e15156ec6
Laptop <dreyui4@gmail.com>
2025-04-15 22:04:03 +0400

    sm4_decrypt function for text decryption by SM4

--- lab_3/function.py
+++ lab_3/function.py
@@ -19,3 +19,22 @@ def sm4_encrypt(key: bytes, text: bytes) -> bytes:
     padded_plaintext = text + bytes([pad_length] * pad_length)
     ciphertext = encryptor.update(padded_plaintext) + encryptor.finalize()
     return iv + ciphertext
+
+
+def sm4_decrypt(key: bytes, ciphertext: bytes) -> bytes:
+    """
+    Decrypts cipher text using the SM4 algorithm in CBC mode
+    :param key:the decryption key is 16 bytes (128 bits) long
+    :param ciphertext:cipher text
+    :return:decrypted text
+    """
+    if len(key) != 16:
+        raise ValueError("The key must be 16 bytes (128 bits)")
+    iv = ciphertext[:16]
+    actual_ciphertext = ciphertext[16:]
+    cipher = Cipher(algorithms.SM4(key), modes.CBC(iv), backend=default_backend())
+    decryptor = cipher.decryptor()
+    padded_text = decryptor.update(actual_ciphertext) + decryptor.finalize()
+    pad_length = padded_text[-1]
+    text = padded_text[:-pad_length]
+    return text
