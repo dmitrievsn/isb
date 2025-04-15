cda09fbaf5c6ced69a17f4480a2e783c35356866
Laptop <dreyui4@gmail.com>
2025-04-16 01:22:13 +0400

    generate_rsa_keys,generates an RSA key pair (private and public)

--- lab_3/function.py
+++ lab_3/function.py
@@ -1,5 +1,6 @@
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
 import os
 
 
@@ -66,3 +67,17 @@ def write_txt_file(data: bytes, file_path: str) -> None:
             file.write(data)
     except Exception as e:
         print(f"Error: {e}")
+
+
+def generate_rsa_keys()->tuple:
+    """
+    Generates an RSA key pair (private and public)
+    :return:RSA private and public keys
+    """
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+        backend=default_backend()
+    )
+    public_key = private_key.public_key()
+    return private_key, public_key
